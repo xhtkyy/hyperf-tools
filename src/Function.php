cac1c26588e83314b77df7bca7f6551ac16f52e7
thompson <so.wo@foxmail.com>
2023-01-13 14:53:51 +0800

    增加类型转换

--- src/Function.php
+++ src/Function.php
@@ -1,6 +1,9 @@
 <?php
 
+use Google\Protobuf\Internal\Message;
+use Google\Protobuf\Struct;
 use Hyperf\Utils\ApplicationContext;
+use function Swoole\Coroutine\map;
 
 if (!function_exists('toArray')) {
     /**
@@ -19,18 +22,26 @@ if (!function_exists('di')) {
 }
 
 if (!function_exists('struct_to_array')) {
-    function struct_to_array(\Google\Protobuf\Struct $struct): array {
+    function struct_to_array(Struct|Message $struct): array {
         return json_decode($struct->serializeToJsonString(), true);
     }
 }
 
 if (!function_exists('array_to_struct')) {
-    function array_to_struct(array $array): \Google\Protobuf\Struct {
-        $struct = new \Google\Protobuf\Struct();
+    function array_to_struct(array $array): Struct {
+        $struct = new Struct();
         try {
             $struct->mergeFromJsonString(json_encode($array));
         } catch (Exception $e) {
         }
         return $struct;
+    }
+}
+
+if (!function_exists("repeated_field_to_array")) {
+    function repeated_field_to_array(\Google\Protobuf\Internal\RepeatedField $repeatedField): array {
+        return map(iterator_to_array($repeatedField), function (Message|Struct $item) {
+            return struct_to_array($item);
+        });
     }
 }
