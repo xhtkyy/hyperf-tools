396063dac3935e4b67c9f8b3eb0fafc1edd20005
thompson <so.wo@foxmail.com>
2023-02-16 14:46:36 +0800

    增加日志打印

--- src/GrpcClient/BaseGrpcClient.php
+++ src/GrpcClient/BaseGrpcClient.php
@@ -6,6 +6,7 @@ namespace Xhtkyy\HyperfTools\GrpcClient;
 use Google\Protobuf\Internal\Message;
 use Hyperf\Context\Context;
 use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\Grpc\StatusCode;
 use Hyperf\Tracer\SpanStarter;
@@ -14,23 +15,27 @@ use Swoole\Http2\Response;
 use Xhtkyy\HyperfTools\App\ContainerInterface;
 use const OpenTracing\Formats\TEXT_MAP;
 
-class BaseGrpcClient {
+class BaseGrpcClient
+{
 
     use SpanStarter;
 
     protected Tracer $tracer;
-
     protected ConfigInterface $config;
+    protected StdoutLoggerInterface $logger;
 
     protected string $hostname = "";
     protected bool $is_tracer = false;
 
-    public function __construct(protected ContainerInterface $container) {
+    public function __construct(protected ContainerInterface $container)
+    {
         $this->tracer = $this->container->get(Tracer::class);
         $this->config = $this->container->get(ConfigInterface::class);
+        $this->logger = $this->container->get(StdoutLoggerInterface::class);
     }
 
-    public function _simpleRequest(string $method, Message $argument, array $deserialize, array $metadata = [], array $options = []): array {
+    public function _simpleRequest(string $method, Message $argument, array $deserialize, array $metadata = [], array $options = []): array
+    {
         $trace_enable = $this->config->get("kyy_tools.trace", true);
         if ($trace_enable && $root = Context::get('tracer.root')) {
             $carrier = [];
@@ -44,16 +49,20 @@ class BaseGrpcClient {
         }
 
         $client = $this->container->get(GrpcClientManager::class);
-        $res    = $client->invoke($this->hostname, $method, $argument, $deserialize, $metadata, $options);
+        $res = $client->invoke($this->hostname, $method, $argument, $deserialize, $metadata, $options);
         /**
          *
          * @var Response $resp
          */
         list($reply, $status) = $res;
         $resp = $res[2] ?? null;
+        // 异常打印
+        if ($status != StatusCode::OK) {
+            !!$reply && $this->logger->error($reply);
+        }
         // 判断是否需要追踪
         if ($trace_enable && $this->is_tracer) {
-            $key  = "GRPC Client Response [RPC] {$method}";
+            $key = "GRPC Client Response [RPC] {$method}";
             $span = $this->startSpan($key);
             $span->setTag('rpc.path', $method);
             if ($resp->headers) foreach ($resp->headers as $key => $value) {
