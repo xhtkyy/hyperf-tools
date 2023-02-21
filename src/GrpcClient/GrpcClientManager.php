cf11b65a93dfa65d0e53a9ba3bda878126066987
thompson <so.wo@foxmail.com>
2023-02-21 09:06:18 +0800

    fix：解决获取节点异常

--- src/GrpcClient/GrpcClientManager.php
+++ src/GrpcClient/GrpcClientManager.php
@@ -119,7 +119,7 @@ class GrpcClientManager
                 $instance = $loadBalancerManager->getInstance($server, $this->algo);
                 if ($instance->removeNode($node) && count($instance->getNodes()) > 0) {
                     $this->logger->debug(sprintf("%s 重试获取节点，%s %s", $server, $hostname, $method));
-                    $this->invoke(null, $method, $argument, $deserialize, $metadata, $options);
+                    return $this->invoke(null, $method, $argument, $deserialize, $metadata, $options);
                 }
             }
             $this->logger->error(sprintf("服务Client获取失败 %s %s,错误信息：%s", $hostname, $method, $e->getMessage()));
