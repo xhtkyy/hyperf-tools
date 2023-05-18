82ed4e10c325802177e3ea77ceb61df9e379fe62
crayxn <crayxn@qq.com>
2023-05-18 15:28:09 +0800

    增加检查db连接

--- src/Function.php
+++ src/Function.php
@@ -10,19 +10,22 @@ if (!function_exists('toArray')) {
      * @param string|array $value
      * @return array
      */
-    function toArray(string|array $value): array {
+    function toArray(string|array $value): array
+    {
         return array_filter(is_array($value) ? $value : (is_string($value) ? explode(",", $value) : []));
     }
 }
 
 if (!function_exists('di')) {
-    function di(string $id) {
+    function di(string $id)
+    {
         return ApplicationContext::getContainer()->get($id);
     }
 }
 
 if (!function_exists('struct_to_array')) {
-    function struct_to_array(Struct|Message $struct): array {
+    function struct_to_array(Struct|Message $struct): array
+    {
         return $struct instanceof Struct ? json_decode($struct->serializeToJsonString(), true) : message_to_array($struct);
     }
 }
@@ -31,12 +34,13 @@ if (!function_exists('message_to_array')) {
     /**
      * proto message 对象转数组,需要消耗性能的
      */
-    function message_to_array(Message $object): array {
+    function message_to_array(Message $object): array
+    {
         $reflectionClass = new ReflectionClass(get_class($object));
-        $array           = array();
+        $array = array();
         foreach ($reflectionClass->getProperties() as $property) {
             $property->setAccessible(true);
-            $value                       = $property->getValue($object);
+            $value = $property->getValue($object);
             if ($value instanceof \Google\Protobuf\Internal\RepeatedField) {
                 // repeated
                 $array[$property->getName()] = repeated_field_to_array($value);
@@ -53,7 +57,8 @@ if (!function_exists('message_to_array')) {
 }
 
 if (!function_exists('array_to_struct')) {
-    function array_to_struct(array $array): Struct {
+    function array_to_struct(array $array): Struct
+    {
         $struct = new Struct();
         try {
             $struct->mergeFromJsonString(json_encode($array));
@@ -64,14 +69,16 @@ if (!function_exists('array_to_struct')) {
 }
 
 if (!function_exists("repeated_field_to_array")) {
-    function repeated_field_to_array(\Google\Protobuf\Internal\RepeatedField $repeatedField): array {
+    function repeated_field_to_array(\Google\Protobuf\Internal\RepeatedField $repeatedField): array
+    {
         return map(iterator_to_array($repeatedField), function (Message|Struct $item) {
             return struct_to_array($item);
         });
     }
 }
 if (!function_exists("array_merge_by_key")) {
-    function array_merge_by_key(array $array, string|int $key, $column = ""): array {
+    function array_merge_by_key(array $array, string|int $key, $column = ""): array
+    {
         $new = [];
         foreach ($array as $item) {
             if (isset($item[$key])) $new[$item[$key]][] = $column != "" ? array_column($item, $column) : $item;
@@ -81,12 +88,35 @@ if (!function_exists("array_merge_by_key")) {
 }
 
 if (!function_exists("check_param_and_call")) {
-    function check_param_and_call(array $param, string|array $fields, callable $fun): void {
+    function check_param_and_call(array $param, string|array $fields, callable $fun): void
+    {
         $fields = is_array($fields) ? $fields : explode(",", $fields);
         foreach ($fields as $field) {
             if (isset($param[$field]) && $param[$field] != "") {
                 $fun($field, $param[$field]);
             }
+        }
+    }
+}
+
+
+if (!function_exists("check_db_connect")) {
+    function check_db_connect($max = 3, $pool = "default"): void
+    {
+        $tryTimes = 0;
+        $output = new \Symfony\Component\Console\Output\ConsoleOutput();
+        while ($tryTimes < $max) {
+            $tryTimes++;
+            $output->writeln("$tryTimes 次尝试连接");
+            try {
+                \Hyperf\DbConnection\Db::connection($pool)->selectOne("select 1");
+            } catch (\Throwable $throwable) {
+                $output->writeln("error: " . $throwable->getMessage());
+                if ($tryTimes == $max) throw $throwable;
+                sleep($tryTimes);
+                continue;
+            }
+            break;
         }
     }
 }
